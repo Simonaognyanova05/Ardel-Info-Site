da2c912d6e22b37d5cdeeaebfcb16dbed9ef3d8d
Simonaognyanova05 <simonaognanova05@gmail.com>
2026-02-08 14:18:01 +0200

    deploy to firebase

--- site/src/services/sendMessage.js
+++ site/src/services/sendMessage.js
@@ -1,23 +1,34 @@
 import { db } from '../config/firebase';
 import { collection, addDoc } from 'firebase/firestore';
+import emailjs from "emailjs-com";
+
+emailjs.init("UGtKXqGnR4WTiD8xP");
 
 export async function sendMessage(name, email, message) {
     try {
-        const docRef = await addDoc(collection(db, "messages"), {
+        // 1️⃣ Запис във Firestore
+        await addDoc(collection(db, "messages"), {
             name,
             email,
             message,
             createdAt: new Date()
         });
 
-        return { status: 200, message: "Съобщението не е изпратено успешно!" };
+        // 2️⃣ Изпращане на имейл
+        await emailjs.send(
+            "service_o8l03ml",
+            "template_p5r3zos",
+            {
+                name,
+                email,
+                message,
+                to_email: "ardel.group@abv.bg"
+            }
+        );
+
+        return { status: 200, message: "Съобщението е изпратено успешно!" };
     } catch (error) {
-        console.error("Грешка при създаване на продукта: ", error.message);
-
-        if (error.message.includes("invalid") || error.message.includes("missing")) {
-            return { status: 400, message: error.message };
-        }
-
-        return { status: 500, message: "Възникна вътрешна грешка!" };
+        console.error("Грешка:", error);
+        return { status: 500, message: "Възникна грешка при изпращането!" };
     }
 }
